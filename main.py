024db6221f99527ab2a4da309654b7f03ab61820
Bosco Domingo <boscodomingob@gmail.com>
2020-08-16 03:02:56 +0200

    Track match improved

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import os
 import sys
+from pprint import pprint
 
 import eyed3
 import spotipy
 import spotipy.util as util
 
-"""
-# TO-DO: Allow users to pick between 2 modes: direct like, or create playlist
-direct_like_scope = 'user-library-modify'
-create_playlist_scope = 'playlist-modify-public'
-"""
-
 
 def get_username():
     """Retrieve username from arguments"""
-    if len(sys.argv) > 1:  # TO-DO Ask for username in terminal
+    if len(sys.argv) > 1:
         return sys.argv[1]
     else:
         print("Usage: `python %s username`" % (sys.argv[0],))
@@ -31,66 +26,79 @@ def connect_to_spotify(username):
 
     if token:
         return spotipy.Spotify(auth=token)
-    raise()
+    print(f"Can't get token for {username}")
+    sys.exit()
 
 
-def get_title_and_artist():
+def get_title_and_artist(rootdir):
     """Recursively reads local files in indicated rootdir and yields a string 'song - artist'"""
-    if(len(rootdir == 0)):
+    if len(rootdir) == 0 or not os.path.isdir(rootdir):
         while True:
             rootdir = input("Please paste the path to your music directory:")
             if os.path.isdir(rootdir):
                 break
             else:
                 print(
-                    "The provided path is not valid. Please try again or type in the path directly\
-                    into the source code if there's issues (use Ctrl + C to exit the program)")
+                    "The provided path is not valid. Please try again or type "
+                    "in the path directly into the source code if there's "
+                    "issues\n(use Ctrl + C to exit the program)")
+    else:
+        print(f"Found valid path. Commencing search in {rootdir}")
 
     successes = 0
     errors = 0
     for subdir, _, files in os.walk(rootdir):
         for file in files:
-            if file.split(".")[1] in formats:
+            if file.split(".")[-1] in formats:
                 # if True:
                 try:
                     audiofile = eyed3.load(os.path.join(subdir, file))
-                    successes += 1
-                    yield str(audiofile.tag.artist, "-", audiofile.tag.title)
                 except:
                     errors += 1
+                else:
+                    yield f"track:{audiofile.tag.title} artist:{audiofile.tag.artist}"
+                    # Query being in double quotes makes it stick to the given
+                    # word order instead of matching a bunch of possibilities
+                    successes += 1
 
-    print(f"Successfully read {successes} and found {errors} errors")
-
-
-def match_songs_to_spotify(song_list):
-    """Checks the previously found list against the Spotify servers"""
-
-
-def add_matches_to_playlist(matches_list):
-    """Puts all matches into one playlist"""
-
-
-def like_matches(song_list):
-    """Likes all the songs that produced a match on Spotify servers """
+    print(f"Successfully read {successes} files and found {errors} errors")
 
 
 if __name__ == "__main__":
-    username = get_username()
-    # username = "Luis De Marcos"
-    try:
-        sp = connect_to_spotify(username)
-    except:
-        print(f"Can't get token for {username}")
-        sys.exit()
-    # Continue with the Spotify object, sp
-    rootdir = ""  # Write the dirpath directly here to avoid having to do it through terminal e.g. '/Users/Luis/Music/iTunes/iTunes Media/Music'
+    """
+    # TO-DO: Allow users to pick between 2 modes: direct like, or create playlist
+    direct_like_scope = 'user-library-modify'
+    create_playlist_scope = 'playlist-modify-public'
+    """
+    rootdir = ""
+    # Write the dirpath directly here to avoid having to do it through terminal
+    # Make sure to escape backslashes. Examples:
+    # '/Users/John/Music/My Music'
+    # "C:\\Users\\John\\Music\\My Music"
     formats = ("mp3", "wav", "flac")
     scope = 'playlist-modify-public user-library-modify user-library-read'
-    tracks = []
-    for query in get_title_and_artist():
-        tracks.append(sp.search(query)["tracks"]['items'][-1]['uri'])
 
-    """ results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print(track['name'] + ' - ' + track['artists'][0]['name']) """
+    username = get_username()
+    sp = connect_to_spotify(username)
+
+    track_IDs = []
+    failed_songs = []
+    searched_songs = 1
+    for query in get_title_and_artist(rootdir):
+        print(f"{searched_songs} - {query}")
+        searched_songs += 1
+        try:
+            # result = sp.search(query, limit=1)
+            result = sp.search(query, limit=1)["tracks"]['items'][0]['id']
+        except:
+            print("\t*NO MATCH*")
+            failed_songs.append(query.replace(
+                "track:", "").replace("artist:", " - ").title())
+            # TO-DO: Write to .txt
+        else:
+            track_IDs.append(result)
+    print(
+        f"TOTAL SONGS SEARCHED: {searched_songs-1}\tTOTAL MATCHES: {len(track_IDs)}")
+    pprint(track_IDs)
+    print("Failed songs:")
+    pprint(failed_songs)
